8ef795c3b0bd795cc5a8399026e0cffcd2ba61ff
Ivan-here <ivan.baranash@gmail.com>
2025-01-24 16:08:38 -0500

    changed lab03.py

--- lab03.py
+++ lab03.py
@@ -1,3 +1,5 @@
+import random
+
 #Dice Options created using list() and range()
 diceOptions = list(range(1,7))
 
@@ -13,3 +15,28 @@ def getCombatStrength(prompt):
             return value
         else:
             print("Invalid Input! Enter a number between 1 to 6")
+
+combatStrength = getCombatStrength("Please enter a number between 1 to 6 for a Player")
+mCombatStrength = getCombatStrength("Please enter a number between 1 to 6 for the Monster")
+
+for j in range(1, 21, 2):
+    heroRoll = random.choice(diceOptions)
+    MonsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons[heroRoll - 1]
+    monsterWeapon = weapons[MonsterRoll - 1]
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + MonsterRoll
+
+    print(f"\n Hero rolled {heroRoll}, Monster rolled {MonsterRoll}")
+    print(f"\n Hero selected {heroWeapon}, Monster selected {monsterWeapon}")
+    print(f"Hero Total Strength: {heroTotal}, Monster total {monsterTotal}")
+
+    if heroTotal > monsterTotal:
+        print("Hero Wins!")
+    elif heroTotal < monsterTotal:
+        print("Monster Wins!")
+    else:
+        print("It's a tie!")
+
