0931b83eebdf3683d7cd54165a172e27b4ac164d
Ivan-here <ivan.baranash@gmail.com>
2025-01-24 15:56:19 -0500

    changed lab03

--- lab03.py
+++ lab03.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#Dice Options created using list() and range()
+diceOptions = list(range(1,7))
+
+#weapon list
+weapons = ["Fist","Knife", "CLub", "Gun", "Bomb", "Nuclear Bomb"]
+
+print("Available Weapons: ",', '.join(weapons))
+
+def getCombatStrength(prompt):
+    while True:
+        value = int(input(prompt))
+        if 1 <= value <= 6:
+            return value
+        else:
+            print("Invalid Input! Enter a number between 1 to 6")
